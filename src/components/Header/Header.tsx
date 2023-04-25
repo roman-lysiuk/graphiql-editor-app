4f8879d9b1ee1fbea6d3254205bba46cbb094356
Andrii Holubkov <folganoid@yahoo.com>
2023-04-25 10:33:44 +0300

    feat: add redux, rtk, store

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { setUser } from '../../store/userSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import cl from './Header.module.scss';
 
 export default function Header() {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
+
+  const testLoginHandler = () => {
+    if (user.id > 0) {
+      dispatch(setUser({ id: 0, login: '', email: '' }));
+    } else {
+      dispatch(setUser({ id: 1, login: 'test', email: 'test@test.test' }));
+    }
+  };
+
   return (
     <header>
       <nav className={cl.nav}>
@@ -13,6 +26,7 @@ export default function Header() {
           <li className={cl.nav__list_item}>
             <NavLink to="/about">About</NavLink>
           </li>
+          <button onClick={testLoginHandler}>{user.id ? 'login' : 'logout'}</button>
         </ul>
       </nav>
     </header>
