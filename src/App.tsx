343b0d67f3ae3f86577ddfb3c074ff5b73dcc6a6
Andrii Holubkov <folganoid@yahoo.com>
2023-04-25 11:26:20 +0300

    feat: vitest, istanul coverage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 import Router from './routes/Router';
 import './styles/vendors.scss';
 import './styles/index.scss';
 
-const App = () => <Router />;
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Router />;
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,29 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
 
+import type { InlineConfig } from 'vitest';
+import type { UserConfig } from 'vite';
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [eslint({ cache: false }), react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    coverage: {
+      provider: 'istanbul',
+      all: true,
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@P': path.resolve(__dirname, 'public'),
     },
   },
-});
+} as VitestConfigExport);
