7b4e9ee9afd44d24a474c19903410fda6a32cd3f
Roman Lysiuk <gekalo1025@gmail.com>
2023-04-30 20:55:30 +0300

    feat:add helpers validate request

---
+++ src/helpers/validateGraphQLRequest.ts
@@ -0,0 +1,27 @@
+function checkСountBrackets(str: string): boolean {
+  let countBracket = 0;
+  const bracketTypeOpen = '{';
+  const bracketTypeClose = '}';
+  str.split('').forEach((letter) => {
+    if (letter === bracketTypeOpen) {
+      countBracket += 1;
+    }
+    if (letter === bracketTypeClose) {
+      countBracket -= 1;
+    }
+  });
+
+  return countBracket === 0;
+}
+
+export default function validateGraphQLRequest(query: string): boolean {
+  if (!query.match(/query/i)) {
+    return false;
+  }
+
+  if (!checkСountBrackets(query)) {
+    return false;
+  }
+
+  return true;
+}
