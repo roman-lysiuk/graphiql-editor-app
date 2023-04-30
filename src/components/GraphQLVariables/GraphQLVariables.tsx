feaeaf1ee913457e4528bea4353a61c6633acbfc
Roman Lysiuk <gekalo1025@gmail.com>
2023-04-30 20:53:26 +0300

    feat:add template graph docs and graph variables

---
+++ src/components/GraphQLVariables/GraphQLVariables.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import cl from './graphQLVariables.module.scss';
+
+export default function GraphQLVariables() {
+  return <section className={cl.variables}>GraphQLVariables</section>;
+}
