c4f591b7b203d2ebc4a69936cdb566e964c2fd28
Roman Lysiuk <gekalo1025@gmail.com>
2023-05-01 21:47:57 +0300

    feat:add graphql variable section

--- src/components/GraphQLVariables/GraphQLVariables.tsx
+++ src/components/GraphQLVariables/GraphQLVariables.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import VariablesEditor from '@monaco-editor/react';
 import cl from './graphQLVariables.module.scss';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { changeVariables } from '../../store/graphQLSlice';
 
 export default function GraphQLVariables() {
-  return <section className={cl.variables}>GraphQLVariables</section>;
+  const { variables } = useAppSelector((state) => state.graphQL);
+  const [isOpenVariables, setIsOpenVariables] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  function toggleOpenVariables() {
+    setIsOpenVariables((prev) => !prev);
+  }
+  return (
+    <section className={cl.variables}>
+      <button onClick={toggleOpenVariables} className={cl.variables__btn}>
+        Query Variables
+      </button>
+      <VariablesEditor
+        value={variables}
+        width="100%"
+        height={isOpenVariables ? '20vh' : '0'}
+        theme="vs-dark"
+        language="graphql"
+        onChange={(value) => {
+          if (value) {
+            dispatch(changeVariables(value));
+          }
+        }}
+      />
+    </section>
+  );
 }
