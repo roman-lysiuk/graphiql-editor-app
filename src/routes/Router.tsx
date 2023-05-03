4d2eb65d35d8ffa15a535058aa3193899ebab72c
Roman Lysiuk <gekalo1025@gmail.com>
2023-05-03 18:25:17 +0300

    fix: merge mistake

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import ErrorPage from './ErrorPage/ErrorPage';
 import Main from './Main/Main';
 import Layout from '../components/Layout/Layout';
 import WelcomePage from './Welcome/Welcome';
 import SignPage from './SignPage/SignPage';
+import { useAppSelector } from '../hooks/redux';
 
 const Router: FunctionComponent = () => {
   const user = useAppSelector((state) => state.user);
@@ -20,6 +20,5 @@ const Router: FunctionComponent = () => {
     </Routes>
   );
 };
-
 
 export default Router;
