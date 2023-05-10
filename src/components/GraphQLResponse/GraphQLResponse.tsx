8c82985d397377099493c92e6df2b33b1475fe5e
Roman Lysiuk <gekalo1025@gmail.com>
2023-05-10 21:46:48 +0300

    fix:change spinner and delete line number on response sections

--- src/components/GraphQLResponse/GraphQLResponse.tsx
+++ src/components/GraphQLResponse/GraphQLResponse.tsx
@@ -4,23 +4,25 @@ import { EditorView } from '@codemirror/view';
 import { useAppSelector } from '../../hooks/redux';
 import normalizeData from '../../helpers/normalizeData';
 import Codemirror from '../CodeMirror/Codemirror';
+import Spinner from '../Spinner/Spinner';
 
 export default function GraphQLResponse() {
   const { error, data, isLoading } = useAppSelector((state) => state.graphQL);
+
   return (
     <>
-      {isLoading && <div>Loading...</div>}
-
+      {isLoading && <Spinner />}
       <section className="response">
         <Codemirror
           editor={false}
           onChange={() => {}}
-          value={error || normalizeData(data)}
+          value={error ? normalizeData(error) : normalizeData(data)}
           extensions={[
             EditorState.readOnly.of(true),
             EditorView.theme({
               '&': { height: '78vh' },
               '.cm-scroller': { overflow: 'auto' },
+              '.cm-gutters': { display: 'none' },
             }),
           ]}
         />
